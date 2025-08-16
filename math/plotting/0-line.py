de3a5aa92c715bf04d9a39adfb9281068b315a54
ayluu-06 <pintosaylin7@gmail.com>
2025-08-16 15:19:53 -0300

    Line Graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,20 +3,18 @@
 0-line.py
 Module that defines a function to plot y = x^3 as a red line
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
     """
-    y is plotted as a solid red line
-    The x-axis ranges from 0 to 10
+    y is plotted as a solid red line, x-axis ranges from 0 to 10
     """
 
-    x = np.arange(0, 11)  # x va de 0 a 10 (el ultimo numero se expluye)
     y = np.arange(0, 11) ** 3  # y = x^3 Podria ser y = x ** 3
 
     plt.figure(figsize=(6.4, 4.8))  # Tamaño de la figura
-    plt.plot(x, y, 'r-')  # 'r-' define la linea roja solida
+    plt.xlim(0, 10)  # x va estrictamente de 0 a 10
+    plt.plot(y, 'r-')  # 'r-' define la linea roja solida
     plt.show()  # Muestra la grafica
