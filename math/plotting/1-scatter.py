29c88f008bdec177e8924dcb8865c9258cf70809
ayluu-06 <pintosaylin7@gmail.com>
2025-08-17 21:35:13 -0300

    Scatter

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Creates a scatter plot of height vs weight
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """
+    Creates a scatter plot of height vs weight
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+    plt.title('Men\'s Height vs Weight')
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    plt.scatter(x, y, c='m', marker='.')
+    plt.show()
+    plt.close()
