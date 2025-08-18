a62f417ae935a4e19b545dcc95da4e224d7d5125
ayluu-06 <pintosaylin7@gmail.com>
2025-08-17 23:32:46 -0300

    Derive

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module to calculate the derivative of a polynomial
+"""
+
+
+def poly_derivative(poly):
+    """
+    Calculate the derivative of a polynomial represented by coefficients list
+    """
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    if not all(isinstance(c, (int, float)) for c in poly):
+        return None
+
+    if len(poly) == 1:
+        return [0]
+
+    derivative = [i * poly[i] for i in range(1, len(poly))]
+
+    return derivative
