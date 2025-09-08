f9b285e40ca6ce44fe662d72f990e35d8c0ca6f4
ayluu-06 <pintosaylin7@gmail.com>
2025-09-07 22:24:37 -0300

    Predict

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+modulo documentado
+"""
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """
+    funcion documentada
+    """
+    return network.predict(data, verbose=verbose)
