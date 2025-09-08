d6cc7517bff751e91d76101cc4c98fd6a38073a7
ayluu-06 <pintosaylin7@gmail.com>
2025-09-07 21:18:20 -0300

    Early Stopping

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+modelo documentado
+"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """
+    funcion documentada
+    """
+    callbacks = []
+    if early_stopping and validation_data is not None:
+        callbacks.append(K.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=patience,
+            restore_best_weights=True
+        ))
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data,
+        callbacks=callbacks if callbacks else None
+    )
+    return history
