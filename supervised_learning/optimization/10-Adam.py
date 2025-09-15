eb04845626f009cd2bbe5eff01f29cf291bab686
ayluu-06 <pintosaylin7@gmail.com>
2025-09-14 21:14:35 -0300

    Adam Upgraded

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Modulo documentado
+"""
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """
+    funcion documentada
+    """
+    return tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon
+    )
