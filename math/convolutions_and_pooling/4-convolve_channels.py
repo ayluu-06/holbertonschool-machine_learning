6b06481e391f46a8a7f26c878abbacbcd89b7267
ayluu-06 <pintosaylin7@gmail.com>
2025-09-28 23:33:33 -0300

    Convolution with Channels

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -19,36 +19,46 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
 
     if isinstance(padding, tuple):
         ph, pw = padding
+        pad_top = ph
+        pad_bottom = 0
+        pad_left = pw
+        pad_right = 0
     elif padding == 'valid':
-        ph, pw = 0, 0
+        pad_top = 0
+        pad_bottom = 0
+        pad_left = 0
+        pad_right = 0
     elif padding == 'same':
         out_h = int(np.ceil(h / sh))
         out_w = int(np.ceil(w / sw))
         pad_h_total = max((out_h - 1) * sh + kh - h, 0)
         pad_w_total = max((out_w - 1) * sw + kw - w, 0)
-        ph = pad_h_total // 2
-        pw = pad_w_total // 2
+        pad_top = (pad_h_total + 1) // 2
+        pad_bottom = pad_h_total - pad_top
+        pad_left = (pad_w_total + 1) // 2
+        pad_right = pad_w_total - pad_left
     else:
         raise ValueError(
-            "padding must be 'same', 'valid', or a (ph, pw) tuple")
+            ("padding must be 'same', 'valid', or a (ph, pw) tuple"))
 
     padded = np.pad(
-        images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+        images,
+        ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right), (0, 0)),
+        mode='constant'
+    )
 
-    H = h + 2 * ph
-    W = w + 2 * pw
+    H = h + pad_top + pad_bottom
+    W = w + pad_left + pad_right
     out_h = (H - kh) // sh + 1
     out_w = (W - kw) // sw + 1
 
-    output = np.zeros((m, out_h, out_w))
+    out = np.zeros((m, out_h, out_w))
 
     for i in range(out_h):
         for j in range(out_w):
             vs = i * sh
-            ve = vs + kh
             hs = j * sw
-            he = hs + kw
-            window = padded[:, vs:ve, hs:he, :]
-            output[:, i, j] = np.sum(window * kernel, axis=(1, 2, 3))
+            window = padded[:, vs:vs + kh, hs:hs + kw, :]
+            out[:, i, j] = np.sum(window * kernel, axis=(1, 2, 3))
 
-    return output
+    return out
