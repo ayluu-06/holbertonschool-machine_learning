9204836beb8eb00e71f0ff029127ab2cb3562882
ayluu-06 <pintosaylin7@gmail.com>
2025-11-02 22:22:37 -0300

    1yolo

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -23,47 +23,48 @@ class Yolo:
 
     def process_outputs(self, outputs, image_size):
         """
-        funcion documentada
+        funcion docuemntada
         """
         image_h, image_w = image_size
-        input_h = self.model.input.shape[1]
-        input_w = self.model.input.shape[2]
+        in_h = int(self.model.input_shape[1])
+        in_w = int(self.model.input_shape[2])
 
         def sigmoid(x):
-            return 1. / (1. + np.exp(-x))
+            return 1.0 / (1.0 + np.exp(-x))
 
         boxes = []
         box_confidences = []
         box_class_probs = []
 
         for i, output in enumerate(outputs):
-            gh, gw, anchor_boxes, _ = output.shape
+            gh, gw, nb, _ = output.shape
 
             t_xy = output[..., 0:2]
             t_wh = output[..., 2:4]
-            t_conf = output[..., 4:5]
-            t_class = output[..., 5:]
+            t_obj = output[..., 4:5]
+            t_cls = output[..., 5:]
 
-            grid_x = np.arange(gw).reshape(1, gw, 1)
-            grid_y = np.arange(gh).reshape(gh, 1, 1)
+            cx = np.tile(np.arange(gw).reshape(1, gw, 1), (gh, 1, nb))
+            cy = np.tile(np.arange(gh).reshape(gh, 1, 1), (1, gw, nb))
 
-            bx = (sigmoid(t_xy[..., 0]) + grid_x) / gw
-            by = (sigmoid(t_xy[..., 1]) + grid_y) / gh
+            anchors_i = self.anchors[i]
+            anchors_i = anchors_i.reshape(1, 1, nb, 2)
+            pw = anchors_i[..., 0]
+            ph = anchors_i[..., 1]
 
-            anchor_wh = self.anchors[i].reshape(1, 1, anchor_boxes, 2)
-            pw = anchor_wh[..., 0]
-            ph = anchor_wh[..., 1]
+            bx = (sigmoid(t_xy[..., 0]) + cx) / gw
+            by = (sigmoid(t_xy[..., 1]) + cy) / gh
 
-            bw = (pw * np.exp(t_wh[..., 0])) / input_w
-            bh = (ph * np.exp(t_wh[..., 1])) / input_h
+            bw = (pw * np.exp(t_wh[..., 0])) / in_w
+            bh = (ph * np.exp(t_wh[..., 1])) / in_h
 
-            x1 = (bx - bw / 2) * image_w
-            y1 = (by - bh / 2) * image_h
-            x2 = (bx + bw / 2) * image_w
-            y2 = (by + bh / 2) * image_h
+            x1 = (bx - bw / 2.0) * image_w
+            y1 = (by - bh / 2.0) * image_h
+            x2 = (bx + bw / 2.0) * image_w
+            y2 = (by + bh / 2.0) * image_h
 
-            boxes.append(np.stack([x1, y1, x2, y2], axis=-1))
-            box_confidences.append(sigmoid(t_conf))
-            box_class_probs.append(sigmoid(t_class))
+            boxes.append(np.stack((x1, y1, x2, y2), axis=-1))
+            box_confidences.append(sigmoid(t_obj))
+            box_class_probs.append(sigmoid(t_cls))
 
         return boxes, box_confidences, box_class_probs
