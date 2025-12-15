2f759a55f6a9d4b835d21e61823bc95b1d7b4c86
ayluu-06 <pintosaylin7@gmail.com>
2025-12-14 22:54:19 -0300

    4bayes

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""modulo documentado"""
+"""
+modulo documentado
+"""
 
 import numpy as np
 from scipy.stats import norm
@@ -8,11 +10,15 @@ GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    """clase documentada"""
+    """
+    clase documentada
+    """
 
     def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
                  xsi=0.01, minimize=True):
-        """funcion documentada"""
+        """
+        funcion documentada
+        """
         self.f = f
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
         self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
@@ -20,9 +26,11 @@ class BayesianOptimization:
         self.minimize = minimize
 
     def acquisition(self):
-        """funcion documentada"""
-        mu, var = self.gp.predict(self.X_s)
-        std = np.sqrt(var)
+        """
+        funcion documentada
+        """
+        mu, sigma = self.gp.predict(self.X_s)
+        std = np.sqrt(sigma)
 
         if self.minimize:
             best = np.min(self.gp.Y)
@@ -31,12 +39,16 @@ class BayesianOptimization:
             best = np.max(self.gp.Y)
             imp = mu - best - self.xsi
 
-        EI = np.zeros(mu.shape)
+        EI = np.zeros_like(mu)
 
         mask = std > 0
-        Z = np.zeros(mu.shape)
+        Z = np.zeros_like(mu)
         Z[mask] = imp[mask] / std[mask]
-        EI[mask] = imp[mask] * norm.cdf(Z[mask]) + std[mask] * norm.pdf(Z[mask])
+
+        EI[mask] = imp[mask] * norm.cdf(Z[mask]) + std[
+            mask] * norm.pdf(Z[mask])
+        EI = np.maximum(EI, 0)
 
         X_next = self.X_s[np.argmax(EI)].reshape(1,)
+
         return X_next, EI
