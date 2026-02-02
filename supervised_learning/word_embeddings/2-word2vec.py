ed37162000d9520054b91b1eb24b46b45c26b724
ayluu-06 <pintosaylin7@gmail.com>
2026-02-01 23:02:56 -0300

    correction 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -3,38 +3,24 @@
 2-word2vec module
 """
 
-from gensim.models import Word2Vec
+import gensim
 
 
-def word2vec_model(
-    sentences,
-    vector_size=100,
-    min_count=5,
-    window=5,
-    negative=5,
-    cbow=True,
-    epochs=5,
-    seed=0,
-    workers=1
-):
+def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
+                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
     funcion documentada
     """
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
-        window=window,
         min_count=min_count,
-        workers=workers,
-        sg=0 if cbow else 1,
+        window=window,
         negative=negative,
-        seed=seed
+        sg=0 if cbow else 1,
+        seed=seed,
+        workers=workers
     )
 
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=epochs
-    )
-
+    model.train(sentences, total_examples=model.corpus_count, epochs=epochs)
     return model
