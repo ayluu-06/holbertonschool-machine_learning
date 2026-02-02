625923c3e5624cb7c0b7946241b48ea8a7e1d087
ayluu-06 <pintosaylin7@gmail.com>
2026-02-01 23:04:24 -0300

    correction 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -12,7 +12,6 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     funcion documentada
     """
     model = gensim.models.Word2Vec(
-        sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
@@ -22,5 +21,6 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         workers=workers
     )
 
+    model.build_vocab(sentences)
     model.train(sentences, total_examples=model.corpus_count, epochs=epochs)
     return model
