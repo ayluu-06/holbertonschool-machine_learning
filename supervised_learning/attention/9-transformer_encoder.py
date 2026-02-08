5380c179f801b59e9118f5282e391bb75b1889bb
ayluu-06 <pintosaylin7@gmail.com>
2026-02-08 18:54:21 -0300

    fix

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -1,51 +1,49 @@
 #!/usr/bin/env python3
-"""
-modulo documentado
-"""
-
+"""modulo documentado"""
 import tensorflow as tf
-import numpy as np
 
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
-    """
-    clase documentada
-    """
+    """clase documentada"""
 
-    def __init__(self, N, dm, h, hidden,
-                 input_vocab, max_seq_len, drop_rate=0.1):
-        """
-        funcion documentada
-        """
+    def __init__(
+        self,
+        N,
+        dm,
+        h,
+        hidden,
+        input_vocab,
+        max_seq_len,
+        drop_rate=0.1
+    ):
+        """funcion documentada"""
         super().__init__()
-
         self.N = N
         self.dm = dm
-
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
 
-        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        pe = positional_encoding(max_seq_len, dm)
+        self.positional_encoding = pe
 
         self.blocks = [
             EncoderBlock(dm, h, hidden, drop_rate)
             for _ in range(N)
         ]
-
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
-        """
-        funcion documentada
-        """
+        """funcion documentada"""
         seq_len = tf.shape(x)[1]
-        x = self.embedding(x)
 
+        x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
 
-        x += self.positional_encoding[:seq_len]
+        pe = tf.convert_to_tensor(self.positional_encoding)
+        pe = tf.cast(pe[:seq_len], x.dtype)
+        x = x + pe
 
         x = self.dropout(x, training=training)
 
