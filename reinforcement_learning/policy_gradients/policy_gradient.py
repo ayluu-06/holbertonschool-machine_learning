ef1f1cd7a95548738466d6925aa145063a5ec13e
ayluu-06 <pintosaylin7@gmail.com>
2026-03-29 19:20:40 -0300

    grad

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -20,11 +20,10 @@ def policy_gradient(state, weight):
     """
     state = state[np.newaxis, :]
     probs = policy(state, weight)
-
     action = np.random.choice(len(probs[0]), p=probs[0])
 
-    dsoftmax = probs.copy()
-    dsoftmax[0, action] -= 1
+    dsoftmax = -probs.copy()
+    dsoftmax[0, action] += 1
 
     grad = np.matmul(state.T, dsoftmax)
 
